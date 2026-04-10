281d6b40b849bbc3962ba911537830d3a4bc6cb9
Ruthru020806 <sruthrakumar2@gmail.com>
2026-04-10 06:30:27 +0000

    DAY 5 : Floyd Algorithm

---
+++ Floyd_Algorithm.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define INF 100
+
+int main()
+{
+    int n;
+    cout << "Enter size (n x n) : ";
+    cin >> n;
+
+    vector<vector<int> > dist(n, vector<int>(n));
+
+    // Input adjacency matrix
+    cout << "Enter Elemnts: " << endl;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> dist[i][j];
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for(int k = 0; k < n; k++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(dist[i][k] < INF && dist[k][j] < INF)
+                {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    // Output result
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(dist[i][j] == INF)
+                cout << "* ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Negative cycle check
+    for(int i = 0; i < n; i++)
+    {
+        if(dist[i][i] < 0)
+        {
+            cout << "Negative cycle detected\n";
+            break;
+        }
+    }
+
+    return 0;
+}
